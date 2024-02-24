1c809c1f6193412ffa1c3f02a1b313707975f95c
HoldenBourg <holden.bourg@mykenzie.snhu.edu>
2024-02-23 23:27:29 -0700

    did follower/following/requests list for account

--- src/app/request-template/request-template.component.ts
+++ src/app/request-template/request-template.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
+import { LocalStorageService } from '../services/local-storage/local-storage.service';
+import { AccountInformationModel } from '../services/models/database-objects/account-information-model';
+import { FollowerModel } from '../services/models/database-objects/follower-model';
+import { RoutingService } from '../services/routing/routing.service';
 
 @Component({
   selector: 'app-request-template',
@@ -8,5 +12,86 @@ import { Component } from '@angular/core';
   styleUrl: './request-template.component.scss'
 })
 export class RequestTemplateComponent {
+  @Input()
+  public requestAccount: FollowerModel = {
+    profilePicture: '',
+    username: ''
+  }
 
+  public routingService: RoutingService = inject(RoutingService);
+  public localStorageService: LocalStorageService = inject(LocalStorageService);
+  public currentUser: AccountInformationModel = this.localStorageService.getInformation('currentUser');
+
+
+  onAcceptUser(username: string) {
+    let requestedUsers: FollowerModel[] = this.currentUser.requests;
+    let newRequestedUsers: FollowerModel[] = [];
+
+    let followedUsers: FollowerModel[] = this.currentUser.followers;
+
+    for(let i = 0; i < requestedUsers.length; i++) {
+      if(requestedUsers[i].username != username) {
+        newRequestedUsers.push(requestedUsers[i]);
+      } else if (requestedUsers[i].username == username) {
+        followedUsers.push(requestedUsers[i]);
+      }
+    }
+
+    let newUser: AccountInformationModel = {
+      profilePicture: this.currentUser.profilePicture,
+      username: this.currentUser.username,
+      password: this.currentUser.password,
+      email: this.currentUser.email,
+      firstName: this.currentUser.firstName,
+      lastName: this.currentUser.lastName,
+      bio: this.currentUser.bio,
+      followers: followedUsers,
+      following: this.currentUser.following,
+      requests: newRequestedUsers,
+      blocked: this.currentUser.blocked,
+      posts: this.currentUser.posts,
+      postsTaggedIn: this.currentUser.postsTaggedIn,
+      private: this.currentUser.private
+    }
+
+    //update users followedAccounts list in users db
+    this.localStorageService.clearInformation('currentUser');
+    this.localStorageService.setInformation('currentUser', newUser);
+
+    window.location.reload();
+  }
+
+  onDeclineUser(username: string) {
+    let requestedUsers: FollowerModel[] = this.currentUser.requests;
+    let newRequestedUsers: FollowerModel[] = [];
+
+    for(let i = 0; i < requestedUsers.length; i++) {
+      if(requestedUsers[i].username != username) {
+        newRequestedUsers.push(requestedUsers[i]);
+      }
+    }
+
+    let newUser: AccountInformationModel = {
+      profilePicture: this.currentUser.profilePicture,
+      username: this.currentUser.username,
+      password: this.currentUser.password,
+      email: this.currentUser.email,
+      firstName: this.currentUser.firstName,
+      lastName: this.currentUser.lastName,
+      bio: this.currentUser.bio,
+      followers: this.currentUser.followers,
+      following: this.currentUser.following,
+      requests: newRequestedUsers,
+      blocked: this.currentUser.blocked,
+      posts: this.currentUser.posts,
+      postsTaggedIn: this.currentUser.postsTaggedIn,
+      private: this.currentUser.private
+    }
+
+    //update users followedAccounts list in users db
+    this.localStorageService.clearInformation('currentUser');
+    this.localStorageService.setInformation('currentUser', newUser);
+
+    window.location.reload();
+  }
 }
